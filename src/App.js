9e8acf666dee749222252b0a0e29231fc4d86f34
Stanley Chen <stanleywchen@outlook.com>
2024-11-03 17:45:39 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import TopNav from "./Components/TopNav";
+import CatNav from "./Components/CatNav";
+import MainComponent from "./Components/MainComponent";
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+      <CatNav />
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
